2300619ae1fe86ae85d999918b81a3ff17d0db14
fx-moon <fx-moon@qq.com>
2019-08-15 10:12:38 +0800

    max thread

--- PythonApplication1.py
+++ PythonApplication1.py
@@ -15,13 +15,13 @@ subscription_key = "408f9d1576fc44e5b7606fd911d0c5e8"
 assert subscription_key
 vision_base_url = "https://team17.cognitiveservices.azure.com/vision/v2.0/"
 analyze_url = vision_base_url + "describe"
+maxConnection=2
 
 #init
-maxConnection=10
 conn=0
 target=[]
 tasks = []
-image_path='/Users/vincent/Pictures/pics/'
+image_path='./'
 dirs = os.listdir(image_path)
 query = args.q
 print("query: ",query)
@@ -31,23 +31,27 @@ async def GetImInfo(image_path):
     headers = {'Ocp-Apim-Subscription-Key': subscription_key,
                 'Content-Type': 'application/octet-stream'}
     params = {'visualFeatures': 'Categories,Description,Color'}
-    if conn > maxConnection:
-        asyncio.sleep(1)
+
     async with aiohttp.request(method='post',url=analyze_url,headers=headers,params=params,data=image_data) as r:
         analysis = await r.json()
-
     print(analysis["description"]["tags"])
     if query in analysis["description"]["tags"]:
         target.append(image_path)
-
+event_loop = asyncio.get_event_loop()
 for img in dirs:
     if os.path.splitext(img)[1] in [".jpeg",".jpg",".png"]:
         path = image_path + img
+        conn=conn+1
+        if conn > maxConnection:
+            event_loop.run_until_complete(asyncio.gather(*tasks))
+            asyncio.sleep(1)
+            tasks=[]
+            conn=0
         #print(path)
         tasks.append(GetImInfo(path))
 
-event_loop = asyncio.get_event_loop()
-results = event_loop.run_until_complete(asyncio.gather(*tasks))
+
+event_loop.run_until_complete(asyncio.gather(*tasks))
 event_loop.close()
 
 print(target)
